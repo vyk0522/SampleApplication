8930db12b4b70eb920c55aa11b4d1cc554d1f1ae
Vivek S <vyk0522@gmail.com>
2020-04-07 17:01:35 +0530

    Hot fix

--- src/com/onejava/service/Papaya.java
+++ src/com/onejava/service/Papaya.java
@@ -3,6 +3,6 @@ package com.onejava.service;
 public class Papaya implements Fruit {
     @Override
     public void printName() {
-        System.out.println("I am Papaya");
+        System.out.println("I am Papaya fruit");
     }
 }
