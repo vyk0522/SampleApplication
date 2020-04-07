23ef86b790e55f3ca491623a2107324521915036
Vivek S <vyk0522@gmail.com>
2020-04-07 16:09:29 +0530

    Added Fruit and Apple

---
+++ src/com/onejava/Main.java
@@ -0,0 +1,11 @@
+package com.onejava;
+
+import com.onejava.service.Apple;
+import com.onejava.service.Fruit;
+
+public class Main {
+    public static void main(String[] args) {
+        Fruit fruit = new Apple();
+        fruit.printName();
+    }
+}
---
+++ src/com/onejava/service/Fruit.java
@@ -0,0 +1,5 @@
+package com.onejava.service;
+
+public interface Fruit {
+    void printName();
+}
