ad156b527a19adef1bff4f743180b0c23dc3cabb
Vivek S <vyk0522@gmail.com>
2020-04-07 17:05:22 +0530

    Added some junk method and modified

--- src/com/onejava/service/Papaya.java
+++ src/com/onejava/service/Papaya.java
@@ -3,6 +3,10 @@ package com.onejava.service;
 public class Papaya implements Fruit {
     @Override
     public void printName() {
-        System.out.println("I am Papaya");
+        System.out.println("I am Papaya vegetable");
+    }
+
+    public void junkMethod (){
+
     }
 }
