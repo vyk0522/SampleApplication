3dae5989d5b161d257e3c4cc81042e0d2eedf460
Vivek S <vyk0522@gmail.com>
2020-04-08 11:25:50 +0530

    Final code commit

--- src/com/onejava/Main.java
+++ src/com/onejava/Main.java
@@ -3,12 +3,15 @@ package com.onejava;
 import com.onejava.service.Apple;
 import com.onejava.service.Fruit;
 import com.onejava.service.Papaya;
+import com.onejava.service.Pineapple;
 
 public class Main {
     public static void main(String[] args) {
         Fruit fruit = new Apple();
         fruit.printName();
         fruit = new Papaya();
+        fruit.printName();
+        fruit = new Pineapple();
         fruit.printName();
     }
 }
--- src/com/onejava/service/Papaya.java
+++ src/com/onejava/service/Papaya.java
@@ -1,14 +1,8 @@
 package com.onejava.service;
 
 public class Papaya implements Fruit {
-    public void anotherJunk (){
-    
-    }
     @Override
     public void printName() {
         System.out.println("I am Papaya");
-    }
-    public void junk (){
-    
     }
 }
